2628f9ad950f83a516648ed74bb2ef0708d70cac
Harsh Verma <vharshverma18@gmail.com>
2021-12-31 18:59:26 +0530

    gallery page

--- client/src/pages/TeamPage/TeamPage.jsx
+++ client/src/pages/TeamPage/TeamPage.jsx
@@ -1,25 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import { teamdata } from '../../data/teamdata'
-import TeamCard from './../../components/TeamCard/TeamCard';
+import React from 'react'
 
 export default function TeamPage() {
-    const [team, setTeam] = useState([])
-
-    useEffect(() => {
-        setTeam(teamdata)
-    }, [])
     return (
-        <div >
-        <h1 style={{ textAlign: "center", color: "#e62b1e", margin: '40px 0' }}>TEAM</h1>
-        <div className='container' >
-            <div className="row p-0">
-                {
-                    team.map(person => <TeamCard person={person} key={person.sno} />)
-                }
-            </div>
+        <div>
+            Team Page
         </div>
-        <br />
-    </div>
-    );
+    )
 }
-
